a0b1cb1e293da82d5289915b9e0b227250adc429
Kasuga46 <over9131120@gmail.com>
2025-07-07 18:51:21 +0900

    a

--- test_main.py
+++ test_main.py
@@ -3,7 +3,22 @@ from app.main import app
 
 client = TestClient(app)
 
-def test_read_main():
-    response = client.get("/")
-    assert response.status_code == 200
-    assert response.json() == {"message": "Hello, world"}
+def test_hello_world():
+    res = client.get("/")
+    assert res.status_code == 200
+    assert res.json() == {"message": "Hello, world"}
+
+def test_hello_name():
+    res = client.get("/hello?name=Mike")
+    assert res.status_code == 200
+    assert res.json() == {"message": "Hello, Mike"}
+
+def test_hello_default():
+    res = client.get("/hello")
+    assert res.status_code == 200
+    assert res.json() == {"message": "Hello, World"}
+
+def test_hello_empty():
+    res = client.get("/hello?name=")
+    assert res.status_code == 200
+    assert res.json() == {"message": "Hello, "}
